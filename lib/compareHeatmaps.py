fc68a7018eb26bf80f76914a014d821c153763d7
b3nker <benja@DESKTOP-192F4IV>
2021-04-06 11:40:14 +0200

    fix problems

--- lib/compareHeatmaps.py
+++ lib/compareHeatmaps.py
@@ -1,23 +1,33 @@
 import os
 import sys
+import pandas as pd
+import math
+import numpy as np
 
-class HeatMap:
-	cells = set()
-	def __init__(self, pCells, pId):
-		self.cells = pCells
-		self.id_user = pId
+def topsoe_distance(p, q):
+	print(p, q)
+	v=0
+	for i in range(0,len(p)):
+		#print(v)
+		if p[i] == 0 or q[i] == 0:
+			v = v+0
+			continue
+		v = v + (p[i]*math.log((2*p[i])/( p[i]+q[i])) + q[i]* math.log((2*q[i])/( p[i]+q[i])))
+	return v
 
-def topsoeDistance(setCells1, setCells2):
-	return 0
 
-def compareTwoHeatmap(h1, h2):
-	return topsoeDistance(h1.cells,h2.cells)
+def heatmap_to_matrix(path):
+	df = pd.read_csv(path)
+	df.drop(df.columns[[1,2]], axis=1, inplace=True)
+	df = df.to_numpy()
+	return df
 
-heatmapTrain = sys.argv[1]
-heatMapTestDirectory = sys.argv[2]
 
-map_result = {}
+# Inputs
+path_heatmap_test = sys.argv[1]
+path_heatmap_train = sys.argv[2]
 
-for filename in os.listdir(heatMapTestDirectory):
-	if filename.endswith(".csv"):
-		return
+def find(test_row, df_train):
+
+matrix_test = heatmap_to_matrix(path_heatmap_test)
+matrix_train = heatmap_to_matrix(path_heatmap_train)
