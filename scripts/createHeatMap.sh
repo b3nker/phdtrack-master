9c36eee573bc26347af8db0981ef249c567d29eb
erwan-png <erwan.v@free.fr>
2021-04-06 09:55:09 +0200

    nouveaux scripts

---
+++ scripts/createHeatMap.sh
@@ -0,0 +1,16 @@
+#Clean the directories
+rm -r ../result_heatMaps
+
+#Create the directory
+mkdir ../result_heatMaps
+mkdir ../result_heatMaps/data
+
+json="../ap-attack_heatMap.json"  
+workdir=../result_heatMaps
+
+possibleLog="$workdir""//log-XXXXXXXXXXX.txt"
+log=$(mktemp $possibleLog) 
+
+#Run accio
+java -jar  ../accio.jar run -workdir $workdir $json >> $log
+
